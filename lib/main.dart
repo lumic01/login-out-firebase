93673f72957133844a82ac1d9ab16599164542fe
lumic01 <sirlumic@gmail.com>
2025-04-30 11:42:38 +0100

    m

--- lib/main.dart
+++ lib/main.dart
@@ -29,38 +29,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Register"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            TextField(
+              autocorrect: false,
+              enableSuggestions: false,
+              keyboardType: null,
+              decoration: InputDecoration(
+                hintText: "Enter Your Email Here"
+              ),
             ),
-          ],
+            TextField(
+              autocorrect: false,
+              enableSuggestions: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter Your Password Here"
+              ),
+            ),
+            TextButton(onPressed: () {
+              
+            }, child: Text("Register Now"))
+          ]
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      
     );
   }
 }
